b704836c70862233bf3ca6a1023677db017430bc
uriah_ah <109769619+ukaqm@users.noreply.github.com>
2023-12-14 09:21:12 -0700

    cname

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
 sudo certbot -n -d cougarcruise.is404.net --nginx --agree-tos --email uriahmartin100@gmail.com
+sudo certbot -n -d 2-11skillcheck4.is404.net --nginx --agree-tos --email uriahmartin100@gmail.com
